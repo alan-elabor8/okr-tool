8a7254ea4b2a3b9ca27672dd37da7c106e9d6189
Alan Mangroo <Alan.mangroo@elabor8.com.au>
2019-03-01 17:25:16 +1100

    WIP

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import OkrContainer from './containers/OkrContainer';
 import FormContainer from './containers/FormContainer';
 
 class App extends Component {
   render() {
     return (
       <div className="col-md-6">
-        <h3> Sample Form Container </h3>
-        <FormContainer />
+        <h3> OKR Container </h3>
+        <OkrContainer />
       </div>
     );
   }
