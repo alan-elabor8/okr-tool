eec0637a30dae23104d06be1c868c39f93a2ef56
Alan Mangroo <Alan.mangroo@elabor8.com.au>
2019-03-04 15:06:46 +1100

    Add progress

--- src/containers/ViewContainer.jsx
+++ src/containers/ViewContainer.jsx
@@ -3,6 +3,8 @@ import Button from '../components/Button'
 import OkrTable from '../components/OkrTable.jsx'
 import {BootstrapTable,
        TableHeaderColumn} from 'react-bootstrap-table';
+import ReactTable from "react-table";
+import 'react-table/react-table.css'
 
 class ViewContainer extends Component {
   constructor(props) {
@@ -32,11 +34,54 @@ componentDidMount() {
 
 render() {
     console.log("OKRS3" , this.state.okrs);
+
+      const columns = [{
+        Header: 'Id',
+        accessor: 'id', // String-based value accessors!
+        Cell: e =><a href={e.value}> {e.value} </a>
+      },
+      {
+        Header: 'Objective',
+        accessor: 'objective' // String-based value accessors!
+      },
+        {
+          Header: 'Benefit',
+          accessor: 'benefit' // String-based value accessors!
+        },
+        {
+                Header: 'Objective Progress',
+                accessor: 'progress',
+                Cell: row => (
+                  <div
+                    style={{
+                      width: '100%',
+                      height: '100%',
+                      backgroundColor: '#dadada',
+                      borderRadius: '2px'
+                    }}
+                  >
+                    <div
+                      style={{
+                        width: `${row.value}%`,
+                        height: '100%',
+                        backgroundColor: row.value > 66 ? '#85cc00'
+                          : row.value > 33 ? '#ffbf00'
+                          : '#ff2e00',
+                        borderRadius: '2px',
+                        transition: 'all .2s ease-out'
+                      }}
+                    />
+                  </div>
+                )
+              }
+      ]
+
+
     return (
-          <div className="App">
-            <p className="Table-header">OKR List</p>
-            <OkrTable data={this.state.okrs}/>
-          </div>
+
+
+          <ReactTable data={this.state.okrs} columns={columns} defaultPageSize={5} />
+
         );
     }
 }
